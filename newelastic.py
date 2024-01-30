929665ec97adae6559ba23b2a6ad0e06ccd77771
sushmithabhatr <33518244+sushmithabhatr@users.noreply.github.com>
2024-01-30 10:31:03 +0530

    Create newelastic.py

---
+++ newelastic.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch("http://localhost:9200")
+
+doc = {
+    "author": "kimchy",
+    "text": "Elasticsearch: cool. bonsai cool.",
+    "timestamp": datetime.now(),
+}
+resp = es.index(index="test-index", id=1, document=doc)
+print(resp["result"])
+
+resp = es.get(index="test-index", id=1)
+print(resp["_source"])
+
+es.indices.refresh(index="test-index")
+
+resp = es.search(index="test-index", query={"match_all": {}})
+print("Got {} hits:".format(resp["hits"]["total"]["value"]))
+for hit in resp["hits"]["hits"]:
+    print("{timestamp} {author} {text}".format(**hit["_source"]))
